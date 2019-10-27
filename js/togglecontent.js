579a45cc3e93d010fc3b3f72a5fb6225d1edf000
sportsclimbs <gradep@netcmail.com>
2019-10-27 09:23:53 +0000

    Last commit from Tinamou after London w main.css pages 5,6,7 too

---
+++ js/togglecontent.js
@@ -0,0 +1,9 @@
+let hiddenbox = document.getElementById('dropdownbox');
+
+function showBox() {
+
+hiddenbox.style.display == 'block' ? hiddenbox.style.display = 'none'  : hiddenbox.style.display = 'block';
+
+}
+
+
